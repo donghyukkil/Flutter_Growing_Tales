5bdbd367e7e4c275d73a5c5072a68f50406c702f
donghyukkil <asterism90@gmail.com>
2024-08-30 16:06:50 +0900

    feat: navigationBar main.dart에 연결

--- lib/core/widgets/custom_bottom_navigation_bar.dart
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends StatelessWidget {
-  const CustomBottomNavigationBar({super.key});
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
       backgroundColor: Colors.white,
-      items: [
+      selectedItemColor: Colors.white24,
+      items: const [
         BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.black), label: 'home'),
+          icon: Icon(
+            Icons.home,
+            color: Colors.black,
+          ),
+          label: 'home',
+        ),
         BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart, color: Colors.black),
-            label: 'bar_chart'),
+          icon: Icon(
+            Icons.bar_chart,
+            color: Colors.black,
+          ),
+          label: 'bar_chart',
+        ),
         BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today, color: Colors.black),
-            label: 'calendar_today'),
+          icon: Icon(
+            Icons.calendar_today,
+            color: Colors.black,
+          ),
+          label: 'calendar_today',
+        ),
         BottomNavigationBarItem(
-            icon: Icon(Icons.settings, color: Colors.black), label: 'settings'),
+          icon: Icon(
+            Icons.settings,
+            color: Colors.black,
+          ),
+          label: 'settings',
+        ),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,31 @@ import '/core/theme/app_theme.dart';
 import '../ui/views/landing_screen.dart';
 import '../ui/views/profile_screen.dart';
 import '../ui/views/follows_screen.dart';
+import '/core/widgets/custom_bottom_navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    LandingPage(),
+    ProfileScreen(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +42,16 @@ class MyApp extends StatelessWidget {
           return MaterialApp(
             title: 'GrowingTales',
             theme: AppTheme.lightTheme,
-            home: ProfileScreen(),
+            home: Scaffold(
+              appBar: AppBar(
+                title: Text('Growing Tales'),
+              ),
+              body: _pages[_currentIndex],
+              bottomNavigationBar: CustomBottomNavigationBar(
+                currentIndex: _currentIndex,
+                onTap: _onTabTapped,
+              ),
+            ),
           );
         });
   }
