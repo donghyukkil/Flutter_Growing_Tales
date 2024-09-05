3632b5a5236f951c6ceef8b5688f503ee4767d7f
donghyukkil <asterism90@gmail.com>
2024-09-05 16:16:26 +0900

    feat: UserDiaryTile 생성

---
+++ lib/ui/components/user_diary_tile.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../core/widgets/user_info_tile.dart';
+
+class UserDiaryTile extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String region;
+  final String diaryContent;
+  final VoidCallback onFollowPressed;
+
+  const UserDiaryTile({
+    super.key,
+    required this.imageUrl,
+    required this.name,
+    required this.region,
+    required this.diaryContent,
+    required this.onFollowPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: 50.h,
+      ),
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: Colors.black,
+            width: 2,
+          ),
+          left: BorderSide(
+            color: Colors.black,
+            width: 2,
+          ),
+          right: BorderSide(
+            color: Colors.black,
+            width: 2,
+          ),
+          bottom: BorderSide(
+            color: Colors.black,
+            width: 7,
+          ),
+        ),
+        borderRadius: BorderRadius.circular(10.r),
+      ),
+      //todo Container에 height 속성 대신 Expaneded?
+      height: 220.h,
+      child: Column(
+        children: [
+          UserInfoTile(
+            imageUrl: imageUrl,
+            name: name,
+            region: region,
+            buttonText: 'Follow',
+            onButtonPressed: onFollowPressed,
+            showBorder: false,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20.w,
+            ),
+            child: Text(
+              diaryContent,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          ),
+          SizedBox(
+            height: 10.h,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 15.w,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.chat_bubble_outline,
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text('324'),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.favorite_border,
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text('3.2k'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Icon(Icons.ios_share),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
